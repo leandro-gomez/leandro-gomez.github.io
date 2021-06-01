454b7a78893588e83f423cfa7a1054f98c8f1755
Leandro Gomez <leandro.gomez73@protonmail.com>
2021-06-01 16:29:13 -0500

    Move js to a file

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    title: "Leandro Gomez",
+    navlinks: [
+    	{"href": "https://github.com/leandro-gomez/", "text": "Github"},
+    	{"href": "https://www.linkedin.com/in/leandrogzvz/", "text": "Linkedin"}
+    ]
+  }
+})
