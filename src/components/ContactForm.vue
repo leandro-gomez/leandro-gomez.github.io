21c1c4713d62906d43cc9af639d082bf37ff5754
Leandro Gomez <leandro.gomez73@protonmail.com>
2021-06-07 20:50:48 -0500

    Use axios for async submission

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -3,20 +3,20 @@
       <div class="col-8 offset-2" id="contact-form">
         <span class="text-center">
         <h2 class="text-center">Contact me!</h2>
-        <form action="https://formspree.io/f/xzbyrlrg" method="POST" id="contact-form">
+        <form action="https://formspree.io/f/xzbyrlrg" method="POST" id="contact-form" @submit.prevent="onSubmit">
           <div class="form-group">
             <label for="full_name">Your name</label>
-            <input placeholder="Jonh Smith" type="text" class="form-control" name="fullName">
+            <input v-model="fullName" placeholder="Jonh Smith" type="text" class="form-control" name="fullName">
           </div>
 
           <div class="form-group">
             <label for="_replyto">Your email</label>
-            <input placeholder="your_email@example.com" type="email" class="form-control" name="_replyto">
+            <input v-model="email" placeholder="your_email@example.com" type="email" class="form-control" name="_replyto">
           </div>
 
           <div class="form-group">
             <label for="message">Your message</label>
-            <textarea placeholder="Let me know how I can help you" class="form-control"  name="message"></textarea>
+            <textarea v-model="message" placeholder="Let me know how I can help you" class="form-control"  name="message"></textarea>
           </div>
 
           <!-- your other form fields go here -->
@@ -30,7 +30,24 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'ContactForm'
+  name: 'ContactForm',
+  methods: {
+    onSubmit ($event) {
+      var form = $event.target
+      var action = form.action
+      var data = new FormData(form)
+      axios.post(action, data).then(() => { this.$router.push({ path: '/thanks' }) })
+    }
+  },
+  data () {
+    return {
+      fullName: '',
+      email: '',
+      message: ''
+    }
+  }
 }
 </script>
