1e43d7ed7816c1cda2fa1ba41999c32e5b62ed80
Leandro Gomez <leandro.gomez73@protonmail.com>
2021-06-07 19:09:03 -0500

    Remove nav links

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,4 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>\
-    <router-link to="/thanks">Thanks</router-link>
-  </div>
   <router-view/>
 </template>
 
