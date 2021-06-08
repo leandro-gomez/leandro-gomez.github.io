d4d6e2ea56d365802c65fe53e0e36f41bdec614f
Leandro Gomez <leandro.gomez73@protonmail.com>
2021-06-07 20:53:14 -0500

    Change thanks view

--- src/views/Thanks.vue
+++ src/views/Thanks.vue
@@ -5,10 +5,10 @@
         <div class="jumbotron">
           <h1 class="display-4">Thanks for your message!</h1>
           <p class="lead">I'll reply as soon as possible.</p>
+          <p class="lead">In the meantime, feel free to go back to my profile.</p>
           <hr class="my-4">
-          <p>In the meantime, feel free to explore my profile.</p>
           <router-link to="/" v-slot="{ href, navigate }">
-              <a class="btn btn-primary btn-lg" :href="href" @click="navigate">Go to profile</a>
+              <a class="btn btn-info btn-lg" :href="href" @click="navigate">Go to profile</a>
           </router-link>
 
         </div>
